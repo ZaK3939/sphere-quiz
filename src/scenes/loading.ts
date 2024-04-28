3d6ff7ede54a8325a47f1347feee1d0d20c27501
ZaK3939 <tsu.aria.kalei@gmail.com>
2024-04-28 09:50:49 +0400

    feat: update data

--- src/scenes/loading.ts
+++ src/scenes/loading.ts
@@ -4,7 +4,6 @@ import { asyncAnimation, asyncLoad } from 'gate/util';
 import { createPublicClient, createWalletClient, http, custom, Address } from 'viem';
 import { scroll } from 'viem/chains';
 import { getbossHp } from 'gate/scrollCall';
-import { getAttackParametersFromContract } from 'gate/sepoliaCall';
 
 interface MintKeyEvent {
   keyId: string;
@@ -183,8 +182,7 @@ export default class LoadingScene extends BaseScene {
         if (this.walletClient) {
           const bossHp = await getbossHp(); // ボスHPを取得
           console.log('bossHp:', bossHp);
-          const bossStats = await getAttackParametersFromContract();
-          console.log('bossStats:', bossStats);
+
           await this.battleScene.setBattleState(address, Number(bossHp)); // バトルステートを設定
           this.scene.run('battle'); // バトルシーンを開始
         } else {
